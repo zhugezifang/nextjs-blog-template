f09dad4a4adb5c6665c3c49e27295df5a32603c2
zhugezifang <xiaoqiu2017wy@163.com>
2024-12-24 13:27:52 +0000

    feat:theme

--- src/components/navbar-actions.tsx
+++ src/components/navbar-actions.tsx
@@ -1,13 +1,12 @@
 "use client"
 
-import { ThemeToggle } from "./theme-toggle"
+//import { ThemeToggle } from "./theme-toggle"
 import { LanguageSwitcher } from "./language-switcher"
 
 export function NavbarActions() {
 
   return (
     <div className="flex items-center space-x-2">
-      <ThemeToggle />
       <LanguageSwitcher />
     </div>
   )
